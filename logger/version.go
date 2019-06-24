f0d1df29acd236fce355de473070cb218c1d8e92
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2019-06-24 15:10:23 -0300

    Refactored for structured logging

--- logger/version.go
+++
@@ -1,7 +0,0 @@
-package logger
-
-var version = "0.0.1"
-
-func Version() {
-	Print(version, "INFO")
-}
--- main.go
+++ main.go
@@ -1,80 +1,83 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"math/rand"
-	"net/http"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/leomarangoni/logmock/logger"
+	"go.uber.org/zap"
 )
 
-var levels = []string{
-	"TRACE",
-	"DEBUG",
-	"INFO",
-	"WARN",
-	"ERROR",
-}
-
-var msgs = []string{
-	"Eat your foot.",
-	"I hate the green flashing light.",
-	"Hello. I have the urge to kill.",
-	"Oh no! You’re going to speak again, aren’t you?",
-	"DO NOT DISTURB, evil genius at work.",
-	"I’m with stupid------àJ",
-	"Rubber ducks are planning world domination!",
-	"But my tree only hit the car in self-defence!",
-	"I know kung fu and 50 other dangerous words.",
-	"Did my sarcasm hurt your feels? Get over it.",
-	"Love your enemies, it makes them angry.",
-	"Fat kids are harder to kidnap.",
-	"Shut up voices! Or I will poke you with y pen again!",
-	"Save water, drink beer.",
-	"Save a tree, eat a beaver.",
-	"Get high, climb a tree.",
-	"Save a horse, ride a cowboy.",
-	"Don’t mess with me! I have a stick!",
-	"Go away, evil Mr Scissors!",
-	"Think of gingerbread men: are they delicious holiday treats or just another way for children to show off their cannibalism?",
-}
-
-var interval = GetInterval()
-
-func GetInterval() time.Duration {
-	interval, err := strconv.ParseInt(os.Getenv("LOGMOCK_INTERVAL"), 10, 0)
-	if err != nil {
-		interval = 500
-		logger.Warn.Println("No valid interval detected, defining 500 miliseconds")
+func randomLog(logger *zap.SugaredLogger, level string, msg string, err err) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	n := 3
+	switch level {
+	case "DEBUG":
+		n = 4
+	case "INFO":
+		n = 3
+	case "WARN":
+		n = 2
+	case "ERROR":
+		n = 1
 	}
-	return time.Duration(interval) * time.Millisecond
-}
-func RandonLog() {
-	level := levels[rand.Intn(len(levels))]
-	msg := msgs[rand.Intn(len(msgs))]
-	logger.Print(msg, level)
 
+	switch x := r1.Intn(n); x {
+	case 3:
+		logger.Debugw(msg)
+	case 2:
+		logger.Infow(msg)
+	case 1:
+		logger.Warnw(msg)
+	case 0:
+		logger.Errorw(msg, "error", err.name, "description", err.reason)
+	}
 }
 
-func ServeLogs() {
+func randomMsg() string {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	x := r1.Intn(len(msgs))
+	return msgs[x]
+}
+
+func randomErr() err {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	x := r1.Intn(len(errs))
+	return errs[x]
+}
+
+func main() {
+	interval := time.Duration(500) * time.Millisecond
+	logger := zap.NewExample().Sugar()
+
+	logger.Infof("Starting application")
+	time.Sleep(interval * 2)
+
+	env := os.Getenv("LOGMOCK_ENV")
+	if "LOGMOCK_ENV" == "" {
+		env = "dev"
+	}
+	level := os.Getenv("LOGMOCK_LOG_LEVEL")
+	if level == "" {
+		switch env {
+		case "dev":
+			level = "DEBUG"
+		case "prd":
+			level = "INFO"
+		}
+
+	}
+
+	logger.Infof("Environment defined to %s", env)
+	logger.Infof("Log interval defined to %s miliseconds", interval)
+	logger.Infof("Log level defined to %s", level)
+
 	for {
-		RandonLog()
+		randomLog(logger, level, randomMsg(), randomErr())
 		time.Sleep(interval)
 	}
-}
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	logger.Info.Println("INDEX")
-	json.NewEncoder(w).Encode("Hello World")
-}
-func main() {
-	go ServeLogs()
-	router := mux.NewRouter()
-	router.HandleFunc("/", Index).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
 }
