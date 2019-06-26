11d2ed5d258f4ce100ada482a2b572480df74196
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2019-06-25 22:28:37 -0300

    refact

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
+func getEnv(envvar string, fallback string) string {
+	x := os.Getenv(envvar)
+	if x == "" {
+		return fallback
+	} else {
+		return x
+	}
+}
 func randomLog(logger *zap.SugaredLogger, level string, msg string, err err) {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
@@ -49,17 +63,54 @@ func randomErr() err {
 	return errs[x]
 }
 
-func main() {
-	interval := time.Duration(500) * time.Millisecond
+func serveLogs(ms int64, env string, level string) {
+	interval := time.Duration(ms) * time.Millisecond
 	logger := zap.NewExample().Sugar()
 
 	logger.Infof("Starting application")
-	time.Sleep(interval * 2)
+	time.Sleep(interval)
+	logger.Infof("Environment defined to %s", env)
+	time.Sleep(interval)
+	logger.Infof("Log interval defined to %s", interval)
+	time.Sleep(interval)
+	logger.Infof("Log level defined to %s", level)
 
-	env := os.Getenv("LOGMOCK_ENV")
-	if "LOGMOCK_ENV" == "" {
-		env = "dev"
+	for {
+		randomLog(logger, level, randomMsg(), randomErr())
+		time.Sleep(interval)
 	}
+
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Hello World")
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := zap.NewExample().Sugar()
+
+		logger.Infow("Request OK", "Method", r.Method, "URI", r.RequestURI, "RemoteAddr", r.RemoteAddr, "Host", r.Host, "Agent", r.UserAgent())
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	port := getEnv("LOGMOCK_PORT", "8321")
+	env := getEnv("LOGMOCK_ENV", "dev")
+
+	interval, err := strconv.ParseInt(os.Getenv("LOGMOCK_INTERVAL"), 10, 0)
+	if err != nil {
+		interval = 500
+	}
+
 	level := os.Getenv("LOGMOCK_LOG_LEVEL")
 	if level == "" {
 		switch env {
@@ -71,13 +122,11 @@ func main() {
 
 	}
 
-	logger.Infof("Environment defined to %s", env)
-	logger.Infof("Log interval defined to %s miliseconds", interval)
-	logger.Infof("Log level defined to %s", level)
+	go serveLogs(interval, env, level)
 
-	for {
-		randomLog(logger, level, randomMsg(), randomErr())
-		time.Sleep(interval)
-	}
-
+	router := mux.NewRouter()
+	router.HandleFunc("/", HelloWorld)
+	router.HandleFunc("/health", HealthCheckHandler)
+	router.Use(loggingMiddleware)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
